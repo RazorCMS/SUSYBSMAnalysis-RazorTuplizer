99a5a621a17ea1a3fdcb13f2e582e710ac978c6a
SiXie <Si.Xie@cern.ch>
2016-07-11 23:21:46 +0200

    fix input tag on lheInfo

--- python/razorTuplizer_MC.py
+++ python/razorTuplizer_MC.py
@@ -11,7 +11,8 @@ process.load("Configuration.EventContent.EventContent_cff")
 #load input files
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-        '/store/mc/RunIISpring16MiniAODv1/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/PUSpring16_80X_mcRun2_asymptotic_2016_v3_ext1-v1/20000/0017320C-7BFC-E511-9B2D-0CC47A4C8E34.root'
+        #'/store/mc/RunIISpring16MiniAODv1/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/PUSpring16_80X_mcRun2_asymptotic_2016_v3_ext1-v1/20000/0017320C-7BFC-E511-9B2D-0CC47A4C8E34.root'
+        '/store/mc/RunIISpring16MiniAODv1/SMS-T2bH_mSbottom-300_mLSP-1_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/PUSpring16RAWAODSIM_80X_mcRun2_asymptotic_2016_v3-v1/30000/329EF9EB-C217-E611-A0A1-0CC47A6C1818.root'
    )
 )
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
@@ -90,7 +91,7 @@ process.ntuples = cms.EDAnalyzer('RazorTuplizer',
     BadChargedCandidateFilter = cms.InputTag("BadChargedCandidateFilter",""),
     BadMuonFilter = cms.InputTag("BadPFMuonFilter",""),
 
-    lheInfo = cms.InputTag("externalLHEProducer", "", "LHE"),
+    lheInfo = cms.InputTag("externalLHEProducer", "", ""),
     genInfo = cms.InputTag("generator", "", "SIM"),
     puInfo = cms.InputTag("slimmedAddPileupInfo", "", "PAT"), #uncomment if no pre-mixing
     #puInfo = cms.InputTag("mixData", "", "HLT"), #uncomment for samples with pre-mixed pileup
