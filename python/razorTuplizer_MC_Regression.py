efcc73b285a3638641aa5bc2cacb0697ea6e41bf
SiXie <Si.Xie@cern.ch>
2016-12-22 21:32:55 +0100

    add configs for using the sep23 rereco version of the EGamma regressions

---
+++ python/razorTuplizer_MC_Regression.py
@@ -0,0 +1,152 @@
+import FWCore.ParameterSet.Config as cms
+
+#------ Setup ------#
+
+#initialize the process
+process = cms.Process("razorTuplizer")
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.load("PhysicsTools.PatAlgos.producersLayer1.patCandidates_cff")
+process.load("Configuration.EventContent.EventContent_cff")
+
+#load input files
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+        #'/store/mc/RunIISpring16MiniAODv1/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/PUSpring16_80X_mcRun2_asymptotic_2016_v3_ext1-v1/20000/0017320C-7BFC-E511-9B2D-0CC47A4C8E34.root'
+        #'/store/mc/RunIISpring16MiniAODv1/SMS-T2bH_mSbottom-300_mLSP-1_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/PUSpring16RAWAODSIM_80X_mcRun2_asymptotic_2016_v3-v1/30000/329EF9EB-C217-E611-A0A1-0CC47A6C1818.root'
+        '/store/mc/RunIISpring16MiniAODv2/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/PUSpring16RAWAODSIM_80X_mcRun2_asymptotic_2016_miniAODv2_v0_ext1-v1/00000/001B3734-EE39-E611-9D0B-A0000420FE80.root'
+   )
+)
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
+process.MessageLogger.cerr.FwkReport.reportEvery = 100
+
+#TFileService for output 
+process.TFileService = cms.Service("TFileService", 
+    fileName = cms.string("razorNtuple.root"),
+    closeFileFast = cms.untracked.bool(True)
+)
+
+#load run conditions
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.load('Configuration.Geometry.GeometryIdeal_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+
+#------ Declare the correct global tag ------#
+
+process.GlobalTag.globaltag = '80X_mcRun2_asymptotic_2016_TrancheIV_v7'
+
+#------ If we add any inputs beyond standard miniAOD event content, import them here ------#
+
+#process.load('CommonTools.RecoAlgos.HBHENoiseFilterResultProducer_cfi')
+#process.HBHENoiseFilterResultProducer.minZeros = cms.int32(99999)
+#process.HBHENoiseFilterResultProducer.IgnoreTS4TS5ifJetInLowBVRegion=cms.bool(False) 
+#process.HBHENoiseFilterResultProducer.defaultDecision = cms.string("HBHENoiseFilterResultRun2Loose")
+
+process.load('RecoMET.METFilters.BadChargedCandidateFilter_cfi')
+process.BadChargedCandidateFilter.muons = cms.InputTag("slimmedMuons")
+process.BadChargedCandidateFilter.PFCandidates = cms.InputTag("packedPFCandidates")
+process.BadChargedCandidateFilter.taggingMode = cms.bool(True)
+
+process.load('RecoMET.METFilters.BadPFMuonFilter_cfi')
+process.BadPFMuonFilter.muons = cms.InputTag("slimmedMuons")
+process.BadPFMuonFilter.PFCandidates = cms.InputTag("packedPFCandidates")
+process.BadPFMuonFilter.taggingMode = cms.bool(True)
+
+
+#------ Electron MVA Setup ------#
+# define which IDs we want to produce
+from PhysicsTools.SelectorUtils.tools.vid_id_tools import *
+dataFormat = DataFormat.MiniAOD
+switchOnVIDElectronIdProducer(process, dataFormat)
+my_id_modules = ['RecoEgamma.ElectronIdentification.Identification.mvaElectronID_Spring16_GeneralPurpose_V1_cff','RecoEgamma.ElectronIdentification.Identification.mvaElectronID_Spring16_HZZ_V1_cff']
+
+#add them to the VID producer
+for idmod in my_id_modules:
+    setupAllVIDIdsInModule(process,idmod,setupVIDElectronSelection)
+
+#------ EGM Regression Setup ------#
+process.load('EgammaAnalysis.ElectronTools.regressionApplication_cff')
+
+
+#------ Analyzer ------#
+
+#list input collections
+process.ntuples = cms.EDAnalyzer('RazorTuplizer', 
+    isData = cms.bool(False),    
+    useGen = cms.bool(True),
+    isFastsim = cms.bool(False),
+    enableTriggerInfo = cms.bool(True),                                 
+    triggerPathNamesFile = cms.string("SUSYBSMAnalysis/RazorTuplizer/data/RazorHLTPathnames.dat"),
+    eleHLTFilterNamesFile = cms.string("SUSYBSMAnalysis/RazorTuplizer/data/RazorElectronHLTFilterNames.dat"),
+    muonHLTFilterNamesFile = cms.string("SUSYBSMAnalysis/RazorTuplizer/data/RazorMuonHLTFilterNames.dat"),
+    photonHLTFilterNamesFile = cms.string("SUSYBSMAnalysis/RazorTuplizer/data/RazorPhotonHLTFilterNames.dat"),
+
+    vertices = cms.InputTag("offlineSlimmedPrimaryVertices"),
+    
+    muons = cms.InputTag("slimmedMuons"),
+    electrons = cms.InputTag("slimmedElectrons"),
+    taus = cms.InputTag("slimmedTaus"),
+    photons = cms.InputTag("slimmedPhotons"),
+    jets = cms.InputTag("slimmedJets"),
+    jetsPuppi = cms.InputTag("slimmedJetsPuppi"),
+    jetsAK8 = cms.InputTag("slimmedJetsAK8"),
+    mets = cms.InputTag("slimmedMETs"),
+    metsNoHF = cms.InputTag("slimmedMETsNoHF"),
+    metsPuppi = cms.InputTag("slimmedMETsPuppi"),
+    packedPfCands = cms.InputTag("packedPFCandidates"),
+
+    packedGenParticles = cms.InputTag("packedGenParticles"),
+    prunedGenParticles = cms.InputTag("prunedGenParticles"),
+    genJets = cms.InputTag("slimmedGenJets", "", "PAT"),
+
+    triggerBits = cms.InputTag("TriggerResults","","HLT"),
+    triggerPrescales = cms.InputTag("patTrigger"),
+    triggerObjects = cms.InputTag("selectedPatTrigger"),
+    metFilterBits = cms.InputTag("TriggerResults", "", "PAT"),
+    hbheNoiseFilter = cms.InputTag("HBHENoiseFilterResultProducer","HBHENoiseFilterResult"),
+    hbheTightNoiseFilter = cms.InputTag("HBHENoiseFilterResultProducer","HBHENoiseFilterResultRun2Tight"),
+    hbheIsoNoiseFilter = cms.InputTag("HBHENoiseFilterResultProducer","HBHEIsoNoiseFilterResult"),
+    BadChargedCandidateFilter = cms.InputTag("BadChargedCandidateFilter",""),
+    BadMuonFilter = cms.InputTag("BadPFMuonFilter",""),
+
+    lheInfo = cms.InputTag("externalLHEProducer", "", ""),
+    genInfo = cms.InputTag("generator", "", "SIM"),
+    puInfo = cms.InputTag("slimmedAddPileupInfo", "", "PAT"), #uncomment if no pre-mixing
+    #puInfo = cms.InputTag("mixData", "", "HLT"), #uncomment for samples with pre-mixed pileup
+    hcalNoiseInfo = cms.InputTag("hcalnoise", "", "PAT"),
+
+    secondaryVertices = cms.InputTag("slimmedSecondaryVertices", "", "PAT"),
+
+    rhoAll = cms.InputTag("fixedGridRhoAll", "", "RECO"),
+    rhoFastjetAll = cms.InputTag("fixedGridRhoFastjetAll", "", "RECO"),
+    rhoFastjetAllCalo = cms.InputTag("fixedGridRhoFastjetAllCalo", "", "RECO"),
+    rhoFastjetCentralCalo = cms.InputTag("fixedGridRhoFastjetCentralCalo", "", "RECO"),
+    rhoFastjetCentralChargedPileUp = cms.InputTag("fixedGridRhoFastjetCentralChargedPileUp", "", "RECO"),
+    rhoFastjetCentralNeutral = cms.InputTag("fixedGridRhoFastjetCentralNeutral", "", "RECO"),
+
+    beamSpot = cms.InputTag("offlineBeamSpot", "", "RECO"),
+
+    ebRecHits = cms.InputTag("reducedEgamma", "reducedEBRecHits", "PAT"),
+    eeRecHits = cms.InputTag("reducedEgamma", "reducedEERecHits", "PAT"),
+    esRecHits = cms.InputTag("reducedEgamma", "reducedESRecHits", "PAT"),
+    ebeeClusters = cms.InputTag("reducedEgamma", "reducedEBEEClusters", "PAT"),
+    esClusters = cms.InputTag("reducedEgamma", "reducedESClusters", "PAT"),
+    conversions = cms.InputTag("reducedEgamma", "reducedConversions", "PAT"),
+    singleLegConversions = cms.InputTag("reducedEgamma", "reducedSingleLegConversions", "PAT"),
+    gedGsfElectronCores = cms.InputTag("reducedEgamma", "reducedGedGsfElectronCores", "PAT"),
+    gedPhotonCores = cms.InputTag("reducedEgamma", "reducedGedPhotonCores", "PAT"),
+    superClusters = cms.InputTag("reducedEgamma", "reducedSuperClusters", "PAT"),
+
+    lostTracks = cms.InputTag("lostTracks", "", "PAT"),
+    mvaGeneralPurposeValuesMap     = cms.InputTag("electronMVAValueMapProducer:ElectronMVAEstimatorRun2Spring16GeneralPurposeV1Values"),
+    mvaGeneralPurposeCategoriesMap = cms.InputTag("electronMVAValueMapProducer:ElectronMVAEstimatorRun2Spring16GeneralPurposeV1Categories"),
+    mvaHZZValuesMap     = cms.InputTag("electronMVAValueMapProducer:ElectronMVAEstimatorRun2Spring16HZZV1Values"),
+    mvaHZZCategoriesMap = cms.InputTag("electronMVAValueMapProducer:ElectronMVAEstimatorRun2Spring16HZZV1Categories")
+)
+
+#run
+process.p = cms.Path( process.regressionApplication *
+                      process.egmGsfElectronIDSequence *
+                      #process.HBHENoiseFilterResultProducer*
+                      process.BadChargedCandidateFilter*
+                      process.BadPFMuonFilter*
+                      process.ntuples)
